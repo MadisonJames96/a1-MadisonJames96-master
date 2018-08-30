a221ac5ce0fc7fe4cfbd903b6532fcc7ffd4f00c
madisonjames <madison.james1@my.jcu.edu>
2018-08-30 20:23:12 +1000

    Add how many books are in the file

--- assignment1.py
+++ assignment1.py
@@ -8,6 +8,13 @@ GitHub URL: https://github.com/CP1404-2018-2/a1-MadisonJames96
 def main():
     print("Reading Tracker 1.0 - by Madison James")
 
+    # counts number of lines in file
+    count = 0
+    for line in open("books.csv").readlines():
+        count = count + 1
+
+    # print number of books in file at the beginning of program
+    print("{} books loaded from books.csv".format(count))
 
 
 if __name__ == '__main__':
